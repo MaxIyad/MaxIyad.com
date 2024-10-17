3458ea259fbd096210a782e3dacc165341f03510
MaxIyad <hi@maxiyad.com>
2024-10-17 16:38:24 +0100

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
-
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: netlify(),
+    site: 'https://example.com',
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ]
 });
